9a9bc82a0423b022b528a5566cc6a1409e05bfdd
Gabriel <gabrielmenezes1905@gmail.com>
2024-06-25 19:13:01 -0300

    clonagem do repositorio

--- my-vue-app/src/Components/Button/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./styles.css";
-
-function Button({children ,type}) {
-  return <button  type = {type} className="button-container">{children}</button>;
-}
-
-export default Button;
